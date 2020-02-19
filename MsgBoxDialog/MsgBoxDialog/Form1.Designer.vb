24b0f2fd52939b5c347957643531f9e42eaaa0cd
Brian Worsham <brian.worsham@cgifederal.com>
2020-02-19 12:07:53 -0600

    MsgBoxDialog

---
+++ MsgBoxDialog/MsgBoxDialog/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lblTxt = New System.Windows.Forms.Label()
+        Me.TxtValue = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(256, 159)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 38)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lblTxt
+        '
+        Me.lblTxt.AutoSize = True
+        Me.lblTxt.Location = New System.Drawing.Point(148, 116)
+        Me.lblTxt.Name = "lblTxt"
+        Me.lblTxt.Size = New System.Drawing.Size(102, 20)
+        Me.lblTxt.TabIndex = 1
+        Me.lblTxt.Text = "Button Value"
+        '
+        'TxtValue
+        '
+        Me.TxtValue.Location = New System.Drawing.Point(256, 110)
+        Me.TxtValue.Name = "TxtValue"
+        Me.TxtValue.Size = New System.Drawing.Size(131, 26)
+        Me.TxtValue.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TxtValue)
+        Me.Controls.Add(Me.lblTxt)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lblTxt As Label
+    Friend WithEvents TxtValue As TextBox
+End Class
