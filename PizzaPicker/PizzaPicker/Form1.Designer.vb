135c9cb929424238804d13559fa59618f204b394
Brian Worsham <brian.worsham@cgifederal.com>
2020-02-20 09:19:43 -0600

    PizzaPicker

---
+++ PizzaPicker/PizzaPicker/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LstPizza = New System.Windows.Forms.ListBox()
+        Me.ButtonClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(59, 156)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(107, 24)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Pepperoni"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(59, 205)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(118, 24)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "Mushrooms"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(55, 123)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Extra Toppings"
+        '
+        'LstPizza
+        '
+        Me.LstPizza.FormattingEnabled = True
+        Me.LstPizza.ItemHeight = 20
+        Me.LstPizza.Location = New System.Drawing.Point(208, 123)
+        Me.LstPizza.Name = "LstPizza"
+        Me.LstPizza.Size = New System.Drawing.Size(496, 144)
+        Me.LstPizza.TabIndex = 3
+        '
+        'ButtonClear
+        '
+        Me.ButtonClear.Location = New System.Drawing.Point(59, 294)
+        Me.ButtonClear.Name = "ButtonClear"
+        Me.ButtonClear.Size = New System.Drawing.Size(118, 28)
+        Me.ButtonClear.TabIndex = 4
+        Me.ButtonClear.Text = "Clear"
+        Me.ButtonClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ButtonClear)
+        Me.Controls.Add(Me.LstPizza)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Name = "Form1"
+        Me.Text = "PizzaPicker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LstPizza As ListBox
+    Friend WithEvents ButtonClear As Button
+End Class
---
+++ PizzaPicker/PizzaPicker/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+
+    End Sub
+
+    Private Sub ButtonClear_Click(sender As Object, e As EventArgs) Handles ButtonClear.Click
+        LstPizza.Items.Clear()
+
+        If CheckBox1.Checked = True Then
+            LstPizza.Items.Add(CheckBox1.Text)
+        End If
+
+        If CheckBox2.Checked = True Then
+            LstPizza.Items.Add(CheckBox2.Text)
+        End If
+    End Sub
+End Class
