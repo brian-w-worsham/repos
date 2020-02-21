0e69bcc978d66c9b6fd2ed1a41542d9e6824c9ec
Brian Wayne Worsham <brian.w.worsham@hotmail.com>
2020-02-21 16:47:04 -0600

    IfBranching

---
+++ IfBranching/IfBranching/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If (Weekday(Now) = vbSaturday) Or (Weekday(Now) = vbSunday) Then
+            Label1.Text = “Relax – it’s the weekend”
+        Else
+            Label1.Text = “Today’s a working weekday”
+        End If
+    End Sub
+End Class
