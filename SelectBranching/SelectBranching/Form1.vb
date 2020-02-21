c722b3467e1feac626fedfe131a7734a7e4d3677
Brian Wayne Worsham <brian.w.worsham@hotmail.com>
2020-02-21 16:53:38 -0600

    SelectBranching

---
+++ SelectBranching/SelectBranching/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Select Case Weekday(Now)
+            Case Is = vbSaturday
+                Label1.Text = “It’s a Super Saturday”
+            Case Is = vbSunday
+                Label1.Text = “It’s a Lazy Sunday”
+            Case Else
+                Label1.Text = “It’s just another working day”
+        End Select
+    End Sub
+End Class
