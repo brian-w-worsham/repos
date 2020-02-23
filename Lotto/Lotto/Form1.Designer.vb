dc8445b1d5698d6cfa94e586fc60a5f89a922031
Brian Wayne Worsham <brian.w.worsham@hotmail.com>
2020-02-23 13:50:49 -0600

    Lotto

---
+++ Lotto/Lotto/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.BtnPick = New System.Windows.Forms.Button()
+        Me.BtnReset = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(734, 77)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(677, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(620, 77)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(563, 77)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(57, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(506, 77)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(57, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Label5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(449, 77)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(57, 20)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Label6"
+        '
+        'BtnPick
+        '
+        Me.BtnPick.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BtnPick.Location = New System.Drawing.Point(453, 114)
+        Me.BtnPick.Name = "BtnPick"
+        Me.BtnPick.Size = New System.Drawing.Size(194, 40)
+        Me.BtnPick.TabIndex = 6
+        Me.BtnPick.Text = "Get My Luck Numbers"
+        Me.BtnPick.UseVisualStyleBackColor = True
+        '
+        'BtnReset
+        '
+        Me.BtnReset.AutoSize = True
+        Me.BtnReset.Location = New System.Drawing.Point(654, 114)
+        Me.BtnReset.Name = "BtnReset"
+        Me.BtnReset.Size = New System.Drawing.Size(134, 40)
+        Me.BtnReset.TabIndex = 7
+        Me.BtnReset.Text = "Reset"
+        Me.BtnReset.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Lotto.My.Resources.Resources.IMG_0209
+        Me.PictureBox1.Location = New System.Drawing.Point(9, 9)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(411, 242)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(836, 264)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.BtnReset)
+        Me.Controls.Add(Me.BtnPick)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Lotto Number Picker"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents BtnPick As Button
+    Friend WithEvents BtnReset As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Lotto/Lotto/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Sub Clear()
+        Label1.Text = "..."
+        Label2.Text = "..."
+        Label3.Text = "..."
+        Label4.Text = "..."
+        Label5.Text = "..."
+        Label6.Text = "..."
+        BtnPick.Enabled = True
+        BtnReset.Enabled = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Clear()
+        Randomize()
+    End Sub
+
+    Private Sub BtnReset_Click(sender As Object, e As EventArgs) Handles BtnReset.Click
+        Me.Clear()
+    End Sub
+
+    Private Sub BtnPick_Click(sender As Object, e As EventArgs) Handles BtnPick.Click
+        Dim i, r, temp, nums(60) As Integer 'Declaring three variables In one statement
+        For i = 1 To 59
+            nums(i) = i
+        Next
+
+        'This loop will shuffle the values wihin nums elements 1-59
+        For i = 1 To 59
+            r = Int(59 * Rnd()) + 1
+            temp = nums(i)
+            nums(i) = nums(r)
+            nums(r) = temp
+        Next
+
+        Label1.Text = nums(1)
+        Label2.Text = nums(2)
+        Label3.Text = nums(3)
+        Label4.Text = nums(4)
+        Label5.Text = nums(5)
+        Label6.Text = nums(6)
+
+        BtnPick.Enabled = False
+        BtnReset.Enabled = True
+    End Sub
+End Class
