b47e82fce73e80140b0dff210042743775250989
Brian Wayne Worsham <brian.w.worsham@hotmail.com>
2020-02-22 19:44:55 -0600

    RandomGenerator

---
+++ RandomGenerator/RandomGenerator/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Dim num As Integer
+
+    Private Sub GetNumber()
+        num = Math.Ceiling(Rnd() * 20)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+        Me.GetNumber()
+    End Sub
+
+    Private Sub ButtonGuess_Click(sender As Object, e As EventArgs) Handles ButtonGuess.Click
+        Select Case (Val(TextBoxInput.Text))
+            Case Is > num
+                Label1.Text = TextBoxInput.Text & " is too high"
+            Case Is < num
+                Label1.Text = TextBoxInput.Text & " is too low"
+            Case Is = num
+                Label1.Text = TextBoxInput.Text & " is correct. I have thought of another number - Try again!"
+                Me.GetNumber()
+        End Select
+        TextBoxInput.Text = ""
+    End Sub
+End Class
---
+++ RandomGenerator/RandomGenerator/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RandomGenerator")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("RandomGenerator")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("fb5ef471-814f-440b-9a5e-e3abf267b747")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
